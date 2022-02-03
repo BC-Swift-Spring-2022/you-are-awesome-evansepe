02ae08410d3da60ffd570b61c16e96fe733f66ea
evan sepe <sepee@bc.edu>
2022-02-03 18:31:56 -0500

    Video 2.17 Update

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var playSoundSwitch: UISwitch!
     
     var imageNumber = -1
     var messageNumber = -1
@@ -66,8 +67,15 @@ class ViewController: UIViewController {
         
         
         soundNumber = nonRepeatingRandom(originalNumber: soundNumber, upperLimit: totalNumberOfSounds-1)
-        playSound(name: "sound\(soundNumber)")
+        if playSoundSwitch.isOn {
+            playSound(name: "sound\(soundNumber)")
+        }
     }
     
+    @IBAction func playSoundToggled(_ sender: UISwitch) {
+        if !sender.isOn && audioPlayer != nil { //if .isOn is NOT true
+            audioPlayer.stop() //stop playing
+        }
+    }
 }
 
